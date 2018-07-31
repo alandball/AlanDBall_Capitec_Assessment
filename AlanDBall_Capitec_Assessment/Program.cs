8aa0e89c1595322ae1c94a6e007490eeae7a1e2c
Alan <alandball@outlook.com>
2018-07-31 11:44:43 +0200

    Init

---
+++ AlanDBall_Capitec_Assessment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlanDBall_Capitec_Assessment
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string fileName;
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            fileName = fd.FileName;
+            Console.Write(fileName);
+
+
+            var users = new List<string>{"Alan", "Ward", "Martin"};
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+
+            }
+
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
